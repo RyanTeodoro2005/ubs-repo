ebab3f49e5ad0b48938fd083bb874d19b7bb5c4c
Raquel <raquelmoreira847@gmail.com>
2024-11-28 17:33:33 -0300

    Alteração Cadastro

--- frontend/src/app/cadastro/cadastro.component.ts
+++ frontend/src/app/cadastro/cadastro.component.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-cadastro',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './cadastro.component.html',
   styleUrl: './cadastro.component.css'
 })
 export class CadastroComponent {
+ 
+  paciente = {
+    nomeCompleto: '',
+    nomeSocial: '',
+    nomePai: '',
+    nomeMae: '',
+    dataNascimento: '',
+    sexo: '',
+    nacionalidade: '',
+    racaCor: '',
+    contato: '',
+    cpf: '',
+    identidade: ''
+  };
 
+  onSubmit() {
+    console.log('Dados do paciente:', this.paciente);
+  
+  }
 }
