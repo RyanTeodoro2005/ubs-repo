4bea6b1a7f5f465a0af536e8171e89ac2146da6b
RyanTeodoro2005 <ryanbrynasilvateodoro@gmail.com>
2024-12-09 12:57:57 -0300

    Atualização

--- backend/src/main/java/br/sp/gov/fatec/ubs/backend/BackendApplication.java
+++ backend/src/main/java/br/sp/gov/fatec/ubs/backend/BackendApplication.java
@@ -1,8 +1,11 @@
+
 package br.sp.gov.fatec.ubs.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan("br.sp.gov.fatec.ubs.backend.model")
 @SpringBootApplication
 public class BackendApplication {
 
