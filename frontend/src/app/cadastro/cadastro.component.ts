43bfee38e0a7db659795bf5a00001b05d340a79a
RyanTeodoro2005 <ryanbryansilvateodoro@gmail.com>
2024-12-10 16:45:57 -0300

    Avaliação

--- frontend/src/app/cadastro/cadastro.component.ts
+++ frontend/src/app/cadastro/cadastro.component.ts
@@ -3,32 +3,41 @@ import { FormsModule } from '@angular/forms';
 import { Component } from '@angular/core';
 import { Paciente } from '../model/paciente';
 import { PacienteService } from '../service/paciente.service';
-
+ 
 @Component({
   selector: 'app-cadastro',
   standalone: true,
   imports: [CommonModule, FormsModule],
   templateUrl: './cadastro.component.html',
-  styleUrl: './cadastro.component.css',
+  styleUrls: ['./cadastro.component.css'],
   providers: [PacienteService]
 })
-
+ 
 export class CadastroComponent {
   public obj: Paciente = new Paciente();
   public mensagem: string = "";
-
+ 
   constructor(private service: PacienteService) { }
-
-  public salvar(){
+ 
+  public salvar() {
     this.service.salvar(this.obj).subscribe({
-        next:(data)=>{this.mensagem="cliente inserido com sucesso!";},
-        error:(msg)=>{this.mensagem="ocorreu erro tente mais tarde!";}
-     });
-     this.limpar();
+      next: (data) => {
+        this.mensagem = "Paciente inserido com sucesso!";
+       
+        // Exibindo popup de confirmação
+        alert(this.mensagem);
+      },
+      error: (msg) => {
+        this.mensagem = "Ocorreu erro! Tente novamente mais tarde.";
+       
+        // Exibindo popup em caso de erro
+        alert(this.mensagem);
+      }
+    });
+    this.limpar();
   }
-
-  public limpar(){
+ 
+  public limpar() {
     this.obj = new Paciente();
   }
-
 }
