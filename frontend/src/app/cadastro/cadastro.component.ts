31a499c20a0f22f6f6cfd8aa964790d8c8605679
RyanTeodoro2005 <ryanbryansilvateodoro@gmail.com>
2025-02-16 23:19:01 -0300

    Atualização 16/02

--- frontend/src/app/cadastro/cadastro.component.ts
+++ frontend/src/app/cadastro/cadastro.component.ts
@@ -3,7 +3,7 @@ import { FormsModule } from '@angular/forms';
 import { Component } from '@angular/core';
 import { Paciente } from '../model/paciente';
 import { PacienteService } from '../service/paciente.service';
- 
+
 @Component({
   selector: 'app-cadastro',
   standalone: true,
@@ -12,31 +12,31 @@ import { PacienteService } from '../service/paciente.service';
   styleUrls: ['./cadastro.component.css'],
   providers: [PacienteService]
 })
- 
+
 export class CadastroComponent {
   public obj: Paciente = new Paciente();
   public mensagem: string = "";
- 
+
   constructor(private service: PacienteService) { }
- 
+
   public salvar() {
     this.service.salvar(this.obj).subscribe({
       next: (data) => {
         this.mensagem = "Paciente inserido com sucesso!";
-       
+        
         // Exibindo popup de confirmação
         alert(this.mensagem);
       },
       error: (msg) => {
         this.mensagem = "Ocorreu erro! Tente novamente mais tarde.";
-       
+        
         // Exibindo popup em caso de erro
         alert(this.mensagem);
       }
     });
     this.limpar();
   }
- 
+
   public limpar() {
     this.obj = new Paciente();
   }
