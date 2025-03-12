260c23cd9300998d379c5d7b4ed628dd05399dd0
Aluno <aluno@IPIRANGA.FATEC>
2025-03-12 16:19:07 -0300

    Atualização 12/03

--- frontend/src/app/cadastro/cadastro.component.ts
+++ frontend/src/app/cadastro/cadastro.component.ts
@@ -1,8 +1,8 @@
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
 import { Component } from '@angular/core';
 import { Paciente } from '../model/paciente';
 import { PacienteService } from '../service/paciente.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-cadastro',
@@ -12,32 +12,37 @@ import { PacienteService } from '../service/paciente.service';
   styleUrls: ['./cadastro.component.css'],
   providers: [PacienteService]
 })
-
 export class CadastroComponent {
   public obj: Paciente = new Paciente();
   public mensagem: string = "";
+  public isFormSubmitted: boolean = false; // Adicionando a propriedade para controle de envio
 
   constructor(private service: PacienteService) { }
 
   public salvar() {
+    this.isFormSubmitted = true;
+  
+    if (!this.obj.nome || !this.obj.cpf) {
+      return; // Se os campos obrigatórios não estiverem preenchidos, não prossegue
+    }
+  
+    console.log(this.obj); // Adicione esta linha para verificar os dados
+  
     this.service.salvar(this.obj).subscribe({
       next: (data) => {
         this.mensagem = "Paciente inserido com sucesso!";
-        
-        // Exibindo popup de confirmação
         alert(this.mensagem);
+        this.limpar(); // Limpa o formulário após o sucesso
       },
       error: (msg) => {
         this.mensagem = "Ocorreu erro! Tente novamente mais tarde.";
-        
-        // Exibindo popup em caso de erro
         alert(this.mensagem);
       }
     });
-    this.limpar();
   }
 
   public limpar() {
-    this.obj = new Paciente();
+    this.obj = new Paciente(); // Reseta o objeto para um novo paciente
+    this.isFormSubmitted = false; // Reseta o estado de envio
   }
 }
